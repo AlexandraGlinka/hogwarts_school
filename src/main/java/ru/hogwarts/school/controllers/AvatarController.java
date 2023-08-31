96d5772c959af11830ad56d4b580501cf8ba370e
glinka <alexandraglinka@yandex.ru>
2023-08-31 23:34:15 +0300

    avatar list

--- src/main/java/ru/hogwarts/school/controllers/AvatarController.java
+++ src/main/java/ru/hogwarts/school/controllers/AvatarController.java
@@ -15,6 +15,7 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 @RestController
 @RequestMapping("/student")
@@ -58,5 +59,12 @@ public class AvatarController {
 
             is.transferTo(os);
         }
+    }
+
+    @GetMapping("/list-of-avatars")
+    public ResponseEntity<List<Avatar>> getListAvatar(@RequestParam("page") Integer pageNumber,
+                                                        @RequestParam("size") Integer pageSize) {
+        List<Avatar> avatars = avatarService.getAvatarList(pageNumber, pageSize);
+        return ResponseEntity.ok(avatars);
     }
 }
