8a8ebf31de3367f1b65a547f9b0db785468b7395
glinka <alexandraglinka@yandex.ru>
2023-09-10 23:28:15 +0300

    logger

--- src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/FacultyServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.exceptions.FacultyNotFoundException;
 import ru.hogwarts.school.model.Faculty;
@@ -18,28 +20,35 @@ public class FacultyServiceImpl implements FacultyService {
         this.facultyRepository = facultyRepository;
     }
 
+    Logger logger = LoggerFactory.getLogger(FacultyServiceImpl.class);
+
     @Override
     public Faculty addFaculty(Faculty faculty) {
+        logger.debug("Faculty is added: {}", faculty);
         return facultyRepository.save(faculty);
     }
 
     @Override
     public Faculty getFacultyById(Long id) {
+        logger.debug("Get faculty by id {}", id);
         return facultyRepository.findById(id).orElseThrow(() -> new FacultyNotFoundException("Faculty not found"));
     }
 
     @Override
     public Collection<Faculty> getAllFaculties() {
+        logger.debug("Get all faculties");
         return facultyRepository.findAll();
     }
 
     @Override
     public Faculty updateFaculty(Long id, Faculty faculty) {
+        logger.debug("Update faculty by id {}: {}", id, faculty);
         return facultyRepository.save(faculty);
     }
 
     @Override
     public void deleteFaculty(Long id) {
+        logger.debug("Delete faculty by id {}", id);
         facultyRepository.deleteById(id);
     }
 
@@ -50,6 +59,7 @@ public class FacultyServiceImpl implements FacultyService {
 
     @Override
     public Collection<Faculty> findByNameOrColor(String name, String color) {
+        logger.debug("Get faculty by name {} or color {}", name, color);
         return facultyRepository.findByNameIgnoreCaseOrColorIgnoreCase(name, color);
     }
 }
